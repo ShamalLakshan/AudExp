3e1a3de925e22e7f81100c31a9415249dde0e188
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-07-04 10:29:54 +0530

    script

---
+++ Scratcher/main.py
@@ -0,0 +1,8 @@
+import librosa
+import soundfile as sf
+import numpy as np
+import wave
+import matplotlib.pyplot as plt
+
+input_file = "path/to/your/input/audio.wav"
+output_file = "path/to/your/output/vinyl_audio.wav"
