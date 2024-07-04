dd9f1236c18372748f6246357975906474ba2ec1
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-07-04 10:43:08 +0530

    audio and noise

--- Scratcher/main.py
+++ Scratcher/main.py
@@ -4,5 +4,17 @@ import numpy as np
 import wave
 import matplotlib.pyplot as plt
 
-input_file = "path/to/your/input/audio.wav"
-output_file = "path/to/your/output/vinyl_audio.wav"
+
+def add_vinyl_effect(input_file, output_file):
+    # Load the audio file
+    y, sr = librosa.load(input_file, sr=None)
+    
+    # Generate vinyl noise
+    noise_len = len(y)
+    vinyl_noise = np.random.normal(0, 0.005, noise_len)
+
+
+input_file = ""
+output_file = ""
+
+add_vinyl_effect(input_file, output_file)
